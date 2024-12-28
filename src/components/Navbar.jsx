3d2f3059cc60a352ced8f4a21af79c4b653cf623
Rocco Vulpis <roccovulpis@gmail.com>
2024-12-28 16:19:14 -0500

    Directory Update.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../index.css'
 
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <nav>
+        <a href="/"><img src="/src/assets/bill.png" alt="bill" id="nav-logo" /></a>
+        <span id="nav-title">Splitzr</span>
+        <ul id="nav-list">
+            <li><a href="">Split Bill</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Sign Up</a></li>
+        </ul>
+    </nav>
   )
 }
