527c68d065ea568f83b523b4deae139862f3c74e
Rocco Vulpis <roccovulpis@gmail.com>
2024-12-30 22:41:08 -0500

    Button in Home.jsx redirects to bill splitting page when clicked.

--- src/components/HomeMainContent.jsx
+++ src/components/HomeMainContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import tableImg from '../assets/people-at-table.jpg'
 import '../styles/Home.css'
+import { Link } from 'react-router-dom'
 
 export default function HomeMainContent() {
   return (
@@ -8,7 +9,7 @@ export default function HomeMainContent() {
         <div id="home-header-container">
             <h1 id="home-header">Splitzr</h1>
             <h2 id="home-header-desc">Splitting Your Tabs Just Became Effortless With Splitzr!</h2>
-            <button id="home-split-btn">Start Splitting Now</button>
+            <button id="home-split-btn"><Link to="/split-bill">Start Splitting Now</Link></button>
         </div>
         <div id="home-img-container">
             <img src={tableImg} alt="People sitting at the table" />
