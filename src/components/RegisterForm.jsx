52340dae8c8108591b3750ae9910f8b0da8cd9a0
Rocco Vulpis <roccovulpis@gmail.com>
2025-01-21 02:10:11 -0500

    Adjusted register form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -6,18 +6,18 @@ import '../styles/Register.css'
 export default function RegisterForm() {
   return (
     <div className='form'>
-    <form className="register-form">
+      <form className="register-form">
         <h1>Register</h1>
-        <div className='name-fields'>
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
+        <div className='name-input'>
+          <input className='first-name-input' type="text" placeholder="First Name" />
+          <input className='last-name-input' type="text" placeholder="Last Name" />
         </div>
         <input type="password" placeholder="Password" />
         <input type="confirm-password" placeholder="Confirm Password" />
         <input type="text" placeholder="Email" />
         <button id='create-btn'>Create Account</button>
         <p className="message">Already registered? <Link to={"/login"}>Sign In</Link></p>
-    </form>
-</div>
+      </form>
+    </div>
   )
 }
