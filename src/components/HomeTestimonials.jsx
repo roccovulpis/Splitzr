b91fa5ae5023e63265fc26214b435f7df7327588
Rocco Vulpis <roccovulpis@gmail.com>
2025-02-13 22:30:51 -0500

    .

--- src/components/HomeTestimonials.jsx
+++ src/components/HomeTestimonials.jsx
@@ -6,10 +6,10 @@ export default function HomeTestimonials() {
     <div id="testimonials-container">
         <h2>What Users Are Saying</h2>
         <div className="testimonial">
-          <p>"Splitzr made paying the bill at dinner so much easier!" - Jane D.</p>
+          <p>"Splitzr made paying the bill at dinner so much easier!" - Daniel G.</p>
         </div>
         <div className="testimonial">
-          <p>"No more arguing over who owes what. Love it!" - Mark P.</p>
+          <p>"No more arguing over who owes what. Love it!" - Galo V.</p>
         </div>
       </div>
   )
